d12a6525d708d008f0011a5e581c884527d17ad5
Minh Nguyen <minhh2792.work@gmail.com>
2023-05-22 11:55:54 +0700

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/minevn-studio-header.png',
+      image: 'img/minevn-studio-1080p.png',
       navbar: {
         title: 'MineVN Studio',
         logo: {
